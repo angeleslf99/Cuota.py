ba291298547ce3230dc45a7c1b6d23321bf8603c
angeleslf99 <47312340+angeleslf99@users.noreply.github.com>
2019-03-04 07:16:37 -0600

    directorio de archivo de proyecto

--- recibo/NumeroEntre.py
+++ recibo/NumeroEntre.py
@@ -1,18 +1,21 @@
-class NumeroEntre:
-    __numero = float(0)
+'''creamos un metodo que nos pida un mumero entre en 0 y el 9,999 y decir cuantas cifras tiene'''
+
+class NumeroEntre: #este es el nombre de nuestra clase
+    __numero = float(0) #Declaramos los atributos
     __cifras = int(0)
 
-    def __init__(self, numero):
+    def __init__(self, numero): #declaramos el metodo constructor
         self.__numero = numero
-        return
+        return 0
 
     def NumeroEntre(self):
-        if((self.__numero>=0) & (self.__numero<=9.999)):
-            return 0
+        if((self.__numero>=0) & (self.__numero<=9.999)): #gregamos if para declarar entre que valores sera nuestro rango
+         return 0
 
     def getCantidadCifras(self):
         return self.__cifras
-
+        #esta es nuestra salida, como conclusion afirmo que el proposito de la unidad 1
+        # y unidad 2 se ha cumplido, pues se trata de conocer cada uno de los elementos con los que trabajaremos
 
 
 
